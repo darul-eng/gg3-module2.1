3a68d0f474b21bcd63e7c66695b07423fffb9e46
darul-eng <ikhsand18d@student.unhas.ac.id>
2023-07-12 21:02:03 +0800

    create endpoint request body and write body

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({
         meta: {
@@ -51,6 +53,33 @@ app.get('/query-param', (req, res) => {
         }
     });
 })
+
+app.post('/request-body', (req, res) => {
+    let body = req.body
+    res.json({
+        meta: {
+            status: 200,
+            message: "Success",
+        },
+        data: {
+            message: [body]
+        }
+    });
+});
+
+app.post('/write-body', (req, res) => {
+    res.set('Custom-Header', 'This is a custom header');
+    res.status(201);
+    res.json({
+        meta: {
+            status: 200,
+            message: "Success",
+        },
+        data: {
+            message: `created user`
+        }
+    });
+});
 
 app.use((req, res) => {
     res.json({
